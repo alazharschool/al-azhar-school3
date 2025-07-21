c7f47a82a66ef263ba2d9b26fbd3345b9c3be55c
Islam <al.azhar.school.london@gmail.com>
2025-07-21 23:19:10 +0300

    deleted 2

--- app/blog/arabic-language/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import ArticleCard from "@/components/article-card";
-
-export default function ArabicLanguagePage({ post }: { post: any }) {
-  return (
-    <ArticleCard post={post} key={post.id} />
-  );
-} 
