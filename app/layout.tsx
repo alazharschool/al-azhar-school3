36118a8a39da4788bf7a3e4865bd1d71ddb15d1f
Islam <al.azhar.school.london@gmail.com>
2025-07-22 23:55:30 +0300

    7

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="/output.css" />
       </head>
       <body className={notoSansArabic.className}>
         <UserProvider>
