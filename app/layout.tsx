58208ad97cdfbbb6ca95bcad9c13cba36df25b53
Islam <al.azhar.school.london@gmail.com>
2025-07-22 23:19:43 +0300

    Update stylesheet link path in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="/app/output.css" />
+        <link rel="stylesheet" href="/output.css" />
       </head>
       <body className={notoSansArabic.className}>
         <UserProvider>
