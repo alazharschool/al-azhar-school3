2d364d7499ce0cd0757dc6037e290db9b6c38043
alazharschool <al.azhar.school.london@gmail.com>
2025-07-23 05:43:01 +0300

    Delete app directory

--- app/blog/arabic-language/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ArabicLanguagePage() {
-  return (
-    <div className="text-center p-8">
-      <h2 className="text-xl font-semibold">الصفحة تحت الإنشاء</h2>
-    </div>
-  );
-}
